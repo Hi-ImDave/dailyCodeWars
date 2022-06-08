f717ab600f916092916c23cd5a05c5d1bf862351
David Mott <d.s.mott0808@gmail.com>
2022-06-08 13:01:33 -0700

    refactor with PREP

--- 8Kyu/abbreviateTwoWordName_8.js
+++ 8Kyu/abbreviateTwoWordName_8.js
@@ -1,3 +1,10 @@
+// Write a function to convert a name into initials.
+// Parameters: string, two words with a space between them
+// Return: first letter of each word, capitalized, with a period separating them.
+// Example: input - abbrevName("John Smith") return - 'J.S'
+// Pseudo Code: create a variable named initials to store the first letter and period then iterate through the string until a space is found. Concatenate initials with the element directly after the space. return initials capitalized.
+
+
 function abbrevName(name){
     let initials = name[0] + '.' 
     for(i = 0; i < name.length; i++){
@@ -5,7 +12,7 @@ function abbrevName(name){
         initials += name[i +1]
       }
     }
-    return initials.toUpperCase()
-      
-  
-  }
+    return initials.toUpperCase()  
+}
+
+abbrevName('John Smith')

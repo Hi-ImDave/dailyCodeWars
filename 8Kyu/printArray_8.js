9938916bb4a54bc165498223a02e81f580a92e35
David Mott <d.s.mott0808@gmail.com>
2022-06-10 17:49:38 -0700

    refactor with PREP

--- 8Kyu/printArray_8.js
+++ 8Kyu/printArray_8.js
@@ -1,3 +1,12 @@
+// Printing Array elements with Comma delimiters
+// Parameters: an array of any size. elements may be string or int
+// Return: string with comma delimited element of the array in same order
+// Example: input - printArray(["h","o","l","a"]) return - h,o,l,a
+// Pseudo Code: can just use an array method to convert to string, with no arguments
+
+
 function printArray(array){
     return array.toString()
   }
+
+  printArray(["h","o","l","a"])
